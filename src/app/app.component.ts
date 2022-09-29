08af7c7239ff61cb7d081f7300782306257ee946
d hendrix <104184081+dHendrixSeven@users.noreply.github.com>
2022-09-28 21:37:49 -0500

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 // store state
 export class AppComponent {
-  newMember = "";
-  members:string[] = [];
+  newMember = '';
+  members: string[] = [];
 
   // update state
-  onInput(member:string){
+  onInput(member: string) {
     this.newMember = member;
   }
 
-  addMember(){
+  addMember() {
     this.members.push(this.newMember);
   }
-
 }
-
