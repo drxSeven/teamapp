b40eb67fd07c789dbccc1adfec8db03a48790b8c
d hendrix <104184081+dHendrixSeven@users.noreply.github.com>
2022-09-28 21:27:42 -0500

    list updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// store state
 export class AppComponent {
   newMember = "";
   members:string[] = [];
 
+  // update state
   onInput(member:string){
     this.newMember = member;
   }
